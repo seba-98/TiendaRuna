831ac6fe73d2401742f9546a301606c6a577a59b
sebastian belettieri <sebastianbelettieri1@gmail.com>
2021-12-21 20:14:14 -0300

    add docs

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,12 +5,13 @@ import "firebase/compat/firestore"
 
 
 export const app= firebase.initializeApp({
-  "apiKey": process.env.REACT_APP_API_KEY,
-  "authDomain": process.env.REACT_APP_AUTH_DOMAIN,
-  "projectId": process.env.REACT_APP_PROYECT_ID,
-  "storageBucket": process.env.REACT_APP_STORAGE_BUCKET,
-  "messagingSenderId": process.env.REACT_APP_MESSAGING_SENDER_ID,
-  "appId": process.env.REACT_APP_APP_ID
+  "apiKey": process.env.REACT_APP_FIREBASE_API_KEY,
+  "authDomain": process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  "projectId": process.env.REACT_APP_FIREBASE_PROYECT_ID,
+  "storageBucket": process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  "messagingSenderId": process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  "appId": process.env.REACT_APP_APP_FIREBASE_ID,
+  "locationId": process.env.REACT_APP_FIREBASE_LOCATION_ID,
   });
 
   // Initialize Firebase
